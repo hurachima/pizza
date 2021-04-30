c67522d09dee9b52d95d31fa2eabf2c698a3714e
hurachima <62026617+hurachima@users.noreply.github.com>
2021-04-30 22:04:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(function(){
+    let count = 0;
+    console.log(count);
+    let i = $('#input');
+
+    let btn = $('#add');
+
+    let btn2 = $('#remove');
+
+
+
+
+  $(btn2).on('click',function(){
+      $('.p-item').eq(0).remove();
+   
+
+});
+
+if(count == 20){
+   $(btn).attr('disabled','disabled');
+}else{
+    $(btn).on('click',function(){
+        let p = $('<p class="p-item"></p>').text(i.val());
+        $(i.val(''));
+        $('.form__item').after(p);
+        count++;
+        console.log(count); 
+    });
+    
+}
+
+
+$('input[name="checkbox1"]').click(function(){
+   $('.card-item:not(:eq(0))').toggleClass('active');
+
+});
+
+
+});
+
+function fun1(){
+    let rgn = $('#r1');
+    let par = $('#one');
+    $(par).text(rgn.val()+' '+'грн');
+
+};
+
+
